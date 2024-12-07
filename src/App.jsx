2340fb1eb9a1755e04528281d4edb9c67dc9e9fe
Sakshamgupta110 <sakshamgupta110@gmail.com>
2024-12-07 19:04:36 +0530

    store created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import { Footer, Header } from './components'
 
 function App() {
- console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [loading,setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>app </h1>
-    </>
-  )
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+    ,[]})
+     
+   if(!loading)
+   {
+     return (
+      <div className="min-h-screen flex-wrap content-between bg-gray-600">
+        <div className='w-full block'>
+          <Header/>
+          <main>
+            //handle later
+          </main>
+          <Footer/>
+        </div>
+      </div>
+     )
+   }
+   else
+   {
+    return null;
+   }
 }
 
 export default App
